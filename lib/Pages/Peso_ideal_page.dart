4c7f7a91c60ea3fbd63c105333c8e449e2a8b032
samuel <samuelfsp72@gmail.com>
2024-09-24 21:52:49 -0300

    At

--- lib/Pages/Peso_ideal_page.dart
+++ lib/Pages/Peso_ideal_page.dart
@@ -35,7 +35,59 @@ class _PesoIdealPageState extends State<PesoIdealPage> {
               Colors.pinkAccent,
             ],
           ),
-        ),
+        ), //O child utiliza um único Widget. Por exemplo, dentro de um widget container você irá colocar apenas UM widget texto.
+        //child: Text('Exemplo de child'); Já o children você utiliza quando quer utilizar uma LISTA de widgets.
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const SizedBox(height: 30),
+          const Text("Teste do IMC",
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+              )),
+          const SizedBox(height: 15),
+          const CupertinoTextField(
+            cursorColor: Colors.pinkAccent,
+            padding: EdgeInsets.all(15),
+            placeholder: "Digite o seu peso",
+            prefix: Icon(
+              Icons.fitness_center,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+            placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
+            style: TextStyle(color: Colors.white, fontSize: 14),
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(7),
+                )),
+          ),
+          const SizedBox(height: 5),
+          const CupertinoTextField(
+            cursorColor: Colors.pinkAccent,
+            padding: EdgeInsets.all(15),
+            placeholder: "Digite sua altura",
+            prefix: Icon(
+              Icons.height,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+            placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
+            style: TextStyle(color: Colors.white, fontSize: 14),
+            decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.all(Radius.circular(7))),
+          ),
+          const SizedBox(height: 32),
+          ElevatedButton(
+            onPressed: () {
+              // Ação para calcular o IMC
+            },
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
